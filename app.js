9b4c9d4de8f5d3af592d92698eb28d83739be2d9
Nick Rose <nickfbp@gmail.com>
2021-01-18 15:09:02 -0700

    HTTPS

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
 const mongoose = require('mongoose');
+const { jwtPassport } = require('./authenticate');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
@@ -26,6 +27,15 @@ connect.then(() => console.log('Connect correctly to server'),
   );
 
 var app = express();
+
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
+      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
