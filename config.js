ac1653d1820efbd78a5bd336435eb5967ead6cb8
Nick Rose <nickfbp@gmail.com>
2021-01-19 16:09:45 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890=09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '251505286579993',
+        clientSecret: '49b8a584b7e45a7eee16bac84b479d01'
+    }
 }
