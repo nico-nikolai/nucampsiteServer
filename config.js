515eac06b913d936c979d2b437c68ef15b7c3197
Nick Rose <nickfbp@gmail.com>
2021-01-23 12:19:43 -0700

    Workshop Week 4

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'secretKey': '12345-67890=09876-54321',
+    'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/nucampsite',
     'facebook': {
         clientId: '251505286579993',
--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -5,8 +5,6 @@ const cors = require('./cors');
 
 const campsiteRouter = express.Router();
 
-// campsiteRouter.use(bodyParser.json()); where did this come from?
-
 campsiteRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
 .get(cors.cors, (req, res, next) => {
